3dab6160e7303091f49f54e81ac88b9edacae8e3
Juan Monsalve <juanchomonsalve92@gmail.com>
2023-10-09 09:39:15 -0500

    fix issue with TS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import { AppProps } from "next/app";
 import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import { Session } from 'next-auth';
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
